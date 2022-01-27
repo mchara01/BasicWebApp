4d110450e6321f1f203f9dff43d91027c3e27650
mchara01 <mc921@ic.ac.uk>
2022-01-27 00:23:21 +0000

    new query process

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -30,4 +30,10 @@ public class QueryProcessorTest {
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
+    
+    @Test
+    public void knowsAboutHello() throws Exception {
+        assertThat(queryProcessor.process("Hello"),
+                containsString("bye"));
+    }
 }
